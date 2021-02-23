c2072da3649786ecdc783a1de3faa4c115419ca8
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2021-02-23 15:33:00 -0300

    feat: add factory to create student

---
+++ src/main/java/school/FactoryStudent.java
@@ -0,0 +1,26 @@
+package school;
+
+public class FactoryStudent {
+	private Student student;
+
+	public FactoryStudent withNameCPFEmail(String name, String cpf, String email) {
+		this.student = new Student(new CPF(cpf), name, new Email(email));
+		return this; // use chain
+	}
+
+	public FactoryStudent withPhone(String ddd, String number) {
+		this.student.addPhone(ddd, number);
+		return this; // use chain
+	}
+	
+	public Student create() {
+		return this.student;
+	}
+	
+	public static void main(String[] args) {
+		FactoryStudent factory = new FactoryStudent();
+		factory.withNameCPFEmail("", "", "")
+			.withPhone("", "")
+			.create();
+	}
+}
--- src/main/java/school/Recommendation.java
+++ src/main/java/school/Recommendation.java
@@ -1,12 +1,28 @@
 package school;
 
+import java.time.LocalDateTime;
+
 public class Recommendation {
 
 	private Student indicated;
 	private Student indicator;
+	private LocalDateTime dateRecommendation;
 	
 	public Recommendation(Student indicated, Student indicator) {
 		this.indicated = indicated;
 		this.indicator = indicator;
+		this.dateRecommendation = LocalDateTime.now();
+	}
+
+	public Student getIndicated() {
+		return indicated;
+	}
+
+	public Student getIndicator() {
+		return indicator;
+	}
+
+	public LocalDateTime getDateRecommendation() {
+		return dateRecommendation;
 	}
 }
